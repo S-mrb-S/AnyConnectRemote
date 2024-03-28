6ba16cf2e80cf7fdf99eb308a05b579c9790cdef
MehraB832 <mehrabshafae@gmail.com>
2024-03-28 23:22:05 +0330

    Add MmkvManager, add to Global

---
+++ app/src/main/java/sp/anyconnectremote/util/MmkvManager.java
@@ -0,0 +1,55 @@
+package sp.anyconnectremote.util;
+
+import androidx.annotation.NonNull;
+
+import com.tencent.mmkv.MMKV;
+
+import sp.anyconnectremote.data.Static;
+
+// by Mehrab
+public class MmkvManager {
+
+    @NonNull
+    private final MMKV kv = MMKV.defaultMMKV();
+
+    private final String logKey = "log";
+    private final String isServiceKey = "isServiceStart";
+    @NonNull
+    private String logValue;
+    @NonNull
+    private Boolean isServiceValue;
+
+    public MmkvManager() {
+        isServiceValue = kv.decodeBool(isServiceKey, false);
+
+        try {
+            String newValue = kv.decodeString(logKey, Static.getGlobalData().getDefaultLogString());
+            assert newValue != null;
+            logValue = newValue;
+        } catch (NullPointerException | AssertionError e) {
+            logValue = Static.getGlobalData().getDefaultLogString();
+        }
+    }
+//    synchronized
+
+    @NonNull
+    public synchronized String getLogValue() {
+        return logValue;
+    }
+
+    public void setLogValue(@NonNull String logValue) {
+        kv.encode(logKey, logValue);
+        this.logValue = logValue;
+    }
+
+
+    @NonNull
+    public synchronized Boolean getIsServiceValue() {
+        return isServiceValue;
+    }
+
+    public void setIsServiceValue(@NonNull Boolean isServiceValue) {
+        kv.encode(isServiceKey, isServiceValue);
+        this.isServiceValue = isServiceValue;
+    }
+}
