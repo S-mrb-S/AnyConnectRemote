9d2847da854abb07b008bbd51b6352d03257c4d6
MehraB832 <mehrabshafae@gmail.com>
2024-03-28 12:32:15 +0330

    move to model

--- app/src/main/java/sp/anyconnectremote/model/LogManager.java
+++ app/src/main/java/sp/anyconnectremote/model/LogManager.java
@@ -1,52 +1,48 @@
-package sp.anyconnectremote.util;
+package sp.anyconnectremote.model;
 
 import android.util.Log;
-
-import com.tencent.mmkv.MMKV;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import sp.anyconnectremote.BuildConfig;
+import sp.anyconnectremote.data.Global;
 
 public class LogManager {
-    private static final MMKV kv = MMKV.defaultMMKV();
 
-    public static void saveLog(String str) {
+    public void saveLog(String str) {
+        // No thread work in ViewModel
+//        ExecutorService executor = Executors.newFixedThreadPool(2);
+//        executor.execute(() -> {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z", Locale.getDefault());
             String currentDateAndTime = sdf.format(new Date());
-            String resultLog = getLog() +
+            Global.mViewModel.retrieveLogData();
+            String resultLog = Global.mViewModel.getCurrentTextLogData() +
                     "\n" +
                     "Log[" +
                     currentDateAndTime +
                     "]: " +
                     str;
 
-            kv.encode("log", resultLog);
+            Global.mViewModel.saveLogData(resultLog);
 
             logCat("Log saved: " + str);
-        }catch (Exception e){
+        } catch (Exception e) {
             logCat("Error in saving: " + e, true);
         }
+//        });
+//        executor.shutdown();
     }
 
-    public static String getLog(){
-        try{
-            return kv.decodeString("log", "** ** ** **");
-        }catch (Exception e){
-            logCat("Error in getting: " + e, true);
-            return "";
-        }
-    }
-
-    public static void logCat(String str) {
+    public void logCat(String str) {
         logCat(str, false); // Default value for e is false
     }
-    public static void logCat(String str, boolean logType){
+
+    public void logCat(String str, boolean logType) {
         if (BuildConfig.DEBUG) {
-            if(logType){
+            if (logType) {
                 Log.e("LogManager", str);
             } else Log.d("LogManager", str);
             // Perform additional debug-only operations if needed
