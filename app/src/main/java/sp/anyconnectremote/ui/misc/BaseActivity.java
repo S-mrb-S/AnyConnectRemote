2c4100f7aebfd04bbb0f5180838dc7271f43bfd9
MehraB832 <mehrabshafae@gmail.com>
2024-03-30 17:28:42 +0330

    Added features

--- app/src/main/java/sp/anyconnectremote/ui/misc/BaseActivity.java
+++ app/src/main/java/sp/anyconnectremote/ui/misc/BaseActivity.java
@@ -5,8 +5,24 @@ import android.content.Intent;
 import androidx.activity.result.ActivityResult;
 import androidx.appcompat.app.AppCompatActivity;
 
+import sp.anyconnectremote.data.Global;
+import sp.anyconnectremote.data.Static;
+
 public class BaseActivity extends AppCompatActivity {
     protected final BaseActivityResult<Intent, ActivityResult> activityLauncher =
             BaseActivityResult.registerActivityForResult(this);
 
+    protected final Global data = Static.getGlobalData();
+
+    protected void goToBack() {
+        this.getOnBackPressedDispatcher().onBackPressed();
+    }
+
+    protected void finishAllTask() {
+        this.finishAffinity();
+    }
+
+    protected void finishThisTask() {
+        this.finish();
+    }
 }
