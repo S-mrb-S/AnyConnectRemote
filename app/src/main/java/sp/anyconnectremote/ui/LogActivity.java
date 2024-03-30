ded4399c713809e2b847d46cee6ab11777d22fa2
MehraB832 <mehrabshafae@gmail.com>
2024-03-30 17:23:43 +0330

    Add cleanLog, Toolbar

--- app/src/main/java/sp/anyconnectremote/ui/LogActivity.java
+++ app/src/main/java/sp/anyconnectremote/ui/LogActivity.java
@@ -1,21 +1,60 @@
 package sp.anyconnectremote.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 
 import sp.anyconnectremote.R;
+import sp.anyconnectremote.ui.misc.BaseActivity;
 
-public class LogActivity extends AppCompatActivity {
+public class LogActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, LogFragment.newInstance())
                     .commitNow();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.log_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.clear_all) {
+            data.getLogManager().cleanLog();
+            return true;
+        }
+
+        if (id == R.id.goto_main) {
+            try {
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            } catch (Exception e) {
+                data.getLogManager().saveLog("[goto main] Error: " + e.getMessage());
+            } finally {
+                finishAllTask();
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
